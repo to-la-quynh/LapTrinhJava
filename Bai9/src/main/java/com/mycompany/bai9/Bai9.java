805bbe2d04aee71da82002621d108c025bddf681
Quynh Cao Thi Thanh <caothanhquynh07012003@gmail.com>
2024-05-11 10:45:20 +0700

    Hw b9

---
+++ Bai9/src/main/java/com/mycompany/bai9/Bai9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bai9;
+
+/**
+ *
+ * @author caoth
+ */
+public class Bai9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
